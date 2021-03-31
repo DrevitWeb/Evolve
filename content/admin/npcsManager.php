9a408f77cf0947c66c3df145009182b1b99bfde6
DrevitWeb <quentin.jousset@culture-ion.fr>
2021-03-31 20:17:42 +0200

    Ajout des pages admin

---
+++ content/admin/npcsManager.php
@@ -0,0 +1,14 @@
+<?php
+if(!isset($_SESSION["admin"]) || empty($_SESSION["admin"]))
+{
+    \basics\Session::setAlert("errors", "Vous n'êtes pas connecté !");
+    \basics\Utils::redirect("?p=admin");
+}
+?>
+<link rel="stylesheet" type="text/css" href="res/styles/pages/adminStyle.css"/>
+<h1>Page de gestion des PNJ</h1>
+<a id="logout" class="btn rounded" href="?p=admin/logout">Se déconnecter</a>
+<br/>
+<br/>
+
+
